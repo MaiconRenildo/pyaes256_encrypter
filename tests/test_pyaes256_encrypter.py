b33a5a239144a74379cea55b600ccf5769d0a31b
Maicon Renildo <maicon@lunes.io>
2022-05-19 22:45:21 -0300

    first version

---
+++ tests/test_pyaes256_encrypter.py
@@ -0,0 +1,11 @@
+from pyaes256_encrypter import __version__,encode_text,decode_text
+
+
+def test_version():
+    assert __version__ == '1.0.3'
+
+
+def test_encode_and_decode_text():
+    key = "password"
+    text = "hello world"
+    assert decode_text(encode_text(text,key),key) == text
